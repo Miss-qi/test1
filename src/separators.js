48aeebf26674988d43e960faed5a5f752f95ca51
Miss-qi <1370322806@qq.com>
2016-06-05 23:47:29 +0800

    first commit

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function thousands_separators(num) {
-  
+	var ch = num.toString();
+	var reg = /^([1-9]\d*|[1-9]\d*\.\d+)$/;
+	if(reg.test(ch)){
+		return ch.replace(/^[1-9]\d*/,function(ch){
+			return s.split('').reverse().map(function(v,i){
+			if (i % 3 === 0 && i) return v + ',';
+			return v;
+			}).reverse().join('');
+		}).replace(/\.0+$/,'');
+	}
 }
 
 module.exports = thousands_separators;
