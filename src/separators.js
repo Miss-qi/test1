ba712337ed41543acc495d6af6e72ad8922e6c5c
Miss-qi <1370322806@qq.com>
2016-06-08 23:05:39 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,39 +1,29 @@
 'use strict';
 
 function thousands_separators(num) {
-	var ch = num.toString();
-	var reg = /^[1-9]\d*$/;
-	var reg1 = /^[1-9]\d*\.0+$/;
-	var reg2 = /^[1-9]\d*\.\d*[1-9]+$/;
-	var start=ch.length%3;
-	var temp = ch.substring(0, start);
-	if((reg.test(ch))||(reg1.test(ch))){
-		var i;
-		var tempReg = parseInt(ch);
-		ch = tempReg.toString();
-		for(i=start;i<ch.length;i+3){
-			if (i === 0) {
-			temp = ch.substring(i, i + 3);
-			}
-			else {
-			temp += "," + ch.substring(i, i + 3);
-			}
-		}
-	}else if(reg2.test(ch)){
-		var myArray=new Array()
-		myArray=ch.split(".");
-		ch=myArray[0];
-		for(i=start;i<ch.length;i+3){
-			if (i === 0) {
-			temp = ch.substring(i, i + 3);
-			}
-			else {
-			temp += "," + ch.substring(i, i + 3);
-			}
-		}
-		temp=temp+"."+myArray[1];
-	}
-	return temp;
+   var ch = num.toString();
+   var myArray=[];
+   myArray=ch.split(".");
+   ch=myArray[0];
+
+   var start=ch.length%3;
+   var temp = ch.substring(0, start);
+
+      for(var i=start;i<ch.length;i=i+3){
+         if (i === 0) {
+         temp = ch.substring(i, i + 3);
+         }
+         else {
+         temp += "," + ch.substring(i, i + 3);
+            console.log(temp);
+         }
+   }
+
+   if(myArray[1]) {
+      temp=temp+"."+myArray[1];
+   }
+
+   return temp;
 }
 
 module.exports = thousands_separators;
